bb49ab6da8782f5ac0b7899424da90b5b9587e57
SALES <Adrien.Sales@gmail.com>
2023-03-19 07:31:09 +1100

    Create

---
+++ LATEST.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Diplay latest flags
+sh <(curl https://tea.xyz) +duckdb.org \
+duckdb << EOF
+INSTALL httpfs;
+LOAD httpfs;
+select plage,
+    flag_color,
+    case 
+        when (flag_color = 'BLUE')      THEN '🟦'
+        when (flag_color = 'YELLOW')    THEN '🟨'
+        when (flag_color = 'RED')       THEN '🟥'
+    end as flag_color
+from read_csv_auto('https://bit.ly/3ZCJ1X5') as latest;
+EOF
